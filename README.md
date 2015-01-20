c11a5db70f404b7e5e3d1179f6dde4cc0696eb2a
leonardseymore@gmail.com <leonard.seymore@gmail.com>
2015-01-20 14:48:05 +0200

    Initial checkin

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jcformat
 JavaScript Console.Log like string formatter
+
+I just wanted a short piece of code that could format a string exactly like the console.log APIs
+
+jcformat("moo %o %s %d yo", {x:3}, 'Hello', 123.5)
+"moo {"x":3} Hello 123 yo"
---
+++ jcformat.js
@@ -0,0 +1,68 @@
+/**
+ *  Copyright (c) 2015-2015 Leonard Seymore.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+/*jshint regexp: false*/
+(function(exports, undefined) {
+    'use strict';
+
+    function jcformat() {
+        var str = arguments[0];
+        var re = /%(\w\.?\d*)/g;
+        var result = '';
+        var previousMatch = 0;
+        var previousMatchLength = 0;
+        var replacements = 1;
+        var match = null;
+        while ((match = re.exec(str)) != null) {
+            result += str.substring(previousMatch == 0 ? 0 : previousMatch + match[0].length, match.index);
+            var formatOption = match[1];
+            var arg = arguments[replacements++];
+            var value = null;
+            switch (formatOption) {
+                case 'd':
+                    value = parseInt(arg);
+                break;
+                case 's':
+                    value = arg;
+                break;
+                default:
+                    if (formatOption.slice(0, 1) == 'f') {
+                        value = parseFloat(arg);
+                    } else {
+                        value = JSON.stringify(arg);
+                    }
+                break;
+            }
+            result += value;
+            
+            previousMatch = match.index;
+            previousMatchLength = match[0].length;
+        }
+        if (previousMatch < str.length - previousMatchLength) {
+            result += str.substr(previousMatch == 0 ? 0 : previousMatch + previousMatchLength, str.length);
+        }
+        result.replace(re, '');
+        return result;
+    }
+
+    exports.jcformat = jcformat;
+})(typeof exports !== 'undefined' ? jcformat : window);
